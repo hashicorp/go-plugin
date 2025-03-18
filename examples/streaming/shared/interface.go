18f89f67216218bc66901e1d19fbc4c178eec85f
Radek Simko <radek.simko@gmail.com>
2025-03-18 16:24:01 +0000

    examples: Add streaming example

---
+++ examples/streaming/shared/interface.go
@@ -0,0 +1,14 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+
+package shared
+
+import (
+	"context"
+)
+
+type Streamer interface {
+	Configure(ctx context.Context, path string, chunkSize int64) error
+	Read(ctx context.Context) ([]byte, error)
+	Write(ctx context.Context, b []byte) error
+}
